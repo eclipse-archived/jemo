75e4a7d6a96764302208d759d77cc59a1011300b
Karsten Thoms <karsten.thoms@itemis.de>
2019-10-21 11:28:08 +0200

    Fix typo in comment
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- maven-archetype/src/main/resources/archetype-resources/src/main/java/FixedModuleExample.java
+++ maven-archetype/src/main/resources/archetype-resources/src/main/java/FixedModuleExample.java
@@ -3,7 +3,7 @@ package ${package};
 import org.eclipse.jemo.api.FixedModule;
 import org.eclipse.jemo.api.ModuleLimit;
 
-// For more info on how to implement this class plese read https://www.eclipse.org/jemo/docs.php
+// For more info on how to implement this class please read https://www.eclipse.org/jemo/docs.php
 // An example can be found in https://github.com/eclipse/jemo/blob/master/demos/jemo-trader-app/src/main/java/org/eclipse/jemo/tutorial/market/MarketWatch.java
 public class FixedModuleExample implements FixedModule {
 
